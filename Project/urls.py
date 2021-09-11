1c7648b0a48fd11fdc36b6d4d1d77e75e0793081
Angelina Myagkova <aa.myagkova@gmail.com>
2021-09-11 20:22:05 +0200

    update urls

--- Project/urls.py
+++ Project/urls.py
@@ -21,4 +21,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
